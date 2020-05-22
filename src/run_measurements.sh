6f1d873ca7c308790ab4dbf52c16410014576c97
Victor Lima <lima.p.victor@outlook.com>
2020-05-21 22:13:58 -0300

    Commit inicial

---
+++ src/run_measurements.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+set -o xtrace
+
+MEASUREMENTS=2
+ITERATIONS=5
+INITIAL_SIZE=128
+
+# NAMES=('mandelbrot_seq' 'mandelbrot_pth' 'mandelbrot_omp')
+NAMES=('mandelbrot_seq' 'mandelbrot_pth')
+
+make
+mkdir results
+
+for NAME in ${NAMES[@]}; do
+    mkdir results/$NAME
+    THREADS=1
+    if [ $NAME = 'mandelbrot_seq' ] 
+    then
+        end=1
+    else 
+        end=6
+    fi
+    for ((t=1; t<=$end; t++)); do
+        SIZE=$INITIAL_SIZE
+        for ((i=1; i<=$ITERATIONS; i++)); do
+            echo "Info $THREADS $SIZE $MEASUREMENTS" >> full.log 2>&1
+            echo "Info $THREADS $SIZE $MEASUREMENTS" >> seahorse.log 2>&1
+            echo "Info $THREADS $SIZE $MEASUREMENTS" >> elephant.log 2>&1
+            echo "Info $THREADS $SIZE $MEASUREMENTS" >> triple_spiral.log 2>&1
+            for ((j=1; j<=$MEASUREMENTS; j++)); do
+                ./$NAME -2.5 1.5 -2.0 2.0 $SIZE $THREADS >> full.log 2>&1
+                ./$NAME -0.8 -0.7 0.05 0.15 $SIZE $THREADS >> seahorse.log 2>&1
+                ./$NAME 0.175 0.375 -0.1 0.1 $SIZE $THREADS >> elephant.log 2>&1
+                ./$NAME -0.188 -0.012 0.554 0.754 $SIZE $THREADS >> triple_spiral.log 2>&1
+            done
+            SIZE=$(($SIZE * 2))
+        done
+        THREADS=$(($THREADS * 2))
+    done
+
+    SIZE=$INITIAL_SIZE
+
+    mv *.log results/$NAME
+    rm output.ppm
+done
